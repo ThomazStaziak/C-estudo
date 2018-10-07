df736d356cd0c387906208c43b5f6cd713c3ee91
ThomazStaziak <thostaziak@gmail.com>
2018-10-06 23:45:02 -0300

    create folders

---
+++ section3/condicional.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    int a = 2;
+
+    if (a % 2 == 0) {
+        printf("O número é par\n");
+    } else {
+        printf("O número é impar\n");
+    }
+
+}
