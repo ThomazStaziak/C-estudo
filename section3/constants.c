73d9b0de31549bf559e9568f224a403268c0f773
ThomazStaziak <thostaziak@gmail.com>
2018-10-15 21:29:10 -0300

    add a program who teaches how to set constants

---
+++ section3/constants.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+// setting the constants with their respective values
+#define NUMBER 10
+#define LETTER 'A'
+
+int main () {
+    // printing the constants values
+    printf("%d\n", NUMBER);
+    printf("%c\n", LETTER);
+}
